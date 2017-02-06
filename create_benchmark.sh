6ce83413f3950b030a047329feb923ab0f1eb713
Leland Batey <lelandbatey@gmail.com>
2017-02-05 16:10:25 -0800

    Improve shell script ergonomics

--- create_benchmark.sh
+++ create_benchmark.sh
@@ -7,6 +7,22 @@ function benchmark(){
 	echo "$RAW_RESULTS"
 }
 
+START=100000
+STOP=1000000
+INCREMENT=45000
+
+if [ -n "$2" ]; then
+	START="$2"
+	if [ -z "$3" ]; then
+		echo "Need to arguments for either just 'PROGRAM' or for 'PROGRAM START_PRIME STOP_PRIME INCREMENT'"
+		exit 1;
+	fi
+fi
+if [ -n "$3" ]; then
+	STOP="$3"
+fi
+
+
 echo "Nth Prime, Elapsed (Wall Clock) Time, Maximum Resident Set Size (kbytes)"
 for k in $(seq 0 20);
 do
